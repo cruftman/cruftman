6f733537aa603aa87b7add2c36a3cca0e3ccc623
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2019-10-01 11:39:00 +0200

    2019.10.01 11:38

---
+++ packages/ldap/src/Cruftman/Ldap/Service.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @file src/Cruftman/Ldap/ServiceProvider.php
+ *
+ * This file is part of the Cruftman package
+ *
+ * @author Paweł Tomulik <ptomulik@meil.pw.edu.pl>
+ * @package cruftman\ldap
+ * @license Distributed under MIT license.
+ */
+
+declare(strict_types=1);
+
+namespace Cruftman\Ldap;
+
+class Service
+{
+    public function __construct()
+}
+
+// vim: syntax=php sw=4 ts=4 et:
