e21ab8d181a27936d46a3ef6d80b087ac05253d7
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2019-11-06 10:09:14 +0100

    2019.11.06 10:10

--- src/Cruftman/Ldap/Preset/AuthSource.php
+++ src/Cruftman/Ldap/Preset/AuthSource.php
@@ -13,6 +13,8 @@ declare(strict_types=1);
 
 namespace Cruftman\Ldap\Preset;
 
+use Korowai\Lib\Ldap\Adapter\AdapterInterface;
+use Korowai\Lib\Ldap\Adapter\SearchQueryInterface;
 use Korowai\Lib\Ldap\Exception\LdapException;
 use Cruftman\Support\Traits\ValidatesOptions;
 use Cruftman\Ldap\Service;
@@ -39,6 +41,70 @@ class AuthSource extends AbstractPreset
                             ->setAllowedTypes('connections', 'array')
                             ->setAllowedTypes('bind', ['string', 'array']);
                  });
+    }
+
+    /**
+     * @todo Write documentation.
+     * @param  array $arguments
+     * @return Session[]
+     */
+    public function sessions(array $arguments = []) : array
+    {
+        $service = $this->getLdapService();
+        return array_map(function ($sessionOptions) use ($service) {
+            return $service->session($sessionOptions);
+        }, $this->substOption('sessions', $arguments, []));
+    }
+
+    /**
+     * @todo Write documentation
+     * @param  AdapterInterface $ldap
+     * @param  array $arguments
+     * @return SearchQueryInterface|null
+     */
+    public function createSearchQuery(AdapterInterface $ldap, array $arguments = []) : ?SearchQueryInterface
+    {
+        if (($searchOptions = $this->substOption('search', $arguments)) === null) {
+            return null;
+        }
+        return $this->getLdapService()->search($searchOptions)->createQuery($ldap, $arguments);
+    }
+
+    /**
+     * @todo Write documentation
+     * @param  BindingInterface $ldap
+     * @param  array $arguments
+     */
+    public function attempt(array $arguments = [])
+    {
+        $service = $this->getLdapService();
+        $bindingOptions = $this->getOption('attempt.bind');
+        $binding = $service->binding($bindingOptions);
+
+        $connectionsOptions = $this->getOption('attempt.connections');
+        foreach ($connectionsOptions as $connectionOptions) {
+            $connection = $service->connection($connectionOptions);
+            $ldap = $connection->createLdap($arguments);
+            try {
+                if ($binding->bindLdapInterface($ldap, $arguments) === true) {
+                    // FIXME: this is ugly...
+                    return [
+                        'session' => $service->session(['connection' => $connection, 'binding' => $binding])
+                        'ldap' => $ldap,
+                        'bound' => true
+                    ];
+                }
+            } catch (LdapException $exception) {
+                switch($exception->getCode()) {
+                    case 0x31:  // Invalid credentials
+                    case -1:    // Connection error or such
+                        break;
+                    default:
+                        throw $exception;
+                }
+            }
+        }
+        return null;
     }
 }
 
